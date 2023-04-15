562164727315a440f347b869dd5a3dcbb2c10877
pranav madhu <pranavmadhu73@gmail.com>
2023-04-15 14:19:18 +0530

    added dashboard page ;register page and reset page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Reset from "./pages/Reset/Reset";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -87,7 +87,6 @@ export {
   auth,
   db,
   signInWithGoogle,
-  signInWithEmailAndPassword,
   logInWithEmailAndPassword,
   registerWithEmailAndPassword,
   sendPasswordReset,
